3ad9230a5b4eb8c4c1483495a11add6d11b9603d
LorenaGS <dlgomezs@unal.edu.co>
2015-04-11 20:56:59 -0500

    - Agregados atributos y restricciones a los dominios User y Admin

--- grails-app/domain/foro/User.groovy
+++ grails-app/domain/foro/User.groovy
@@ -2,6 +2,17 @@ package foro
 
 class User {
 
+    String name
+    String lastName
+    int age
+    String userName
+    String password
+
     static constraints = {
+        name(blank: false, nullable: false, size: 3..50)
+        lastName(blank: false, nullable: false, size: 3..50)
+        age(blank: false, nullable: false, min: 14)
+        userName(blank: false, nullable: false, unique: true)
+        password(blank: false, nullable: false, matches: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]+)", minSize: 8)
     }
 }
