0a9d7747d079ee135fecd32d90ec19d96770ad97
Christian <chfjimenezga@unal.edu.co>
2015-04-12 00:36:32 -0500

    Cambios iniciales clases Forum y Regular

--- grails-app/domain/foro/Forum.groovy
+++ grails-app/domain/foro/Forum.groovy
@@ -1,7 +1,14 @@
 package foro
 
 class Forum {
+	
+	String name
+	Date dateCreated
+	String category
 
     static constraints = {
+		name(blank:false,nullable:false,length:3..20,unique:true)
+		dateCreated(blank:false,nullable:false,min:new Date())
+		category(blank:false,nullable:false,length:3..15)
     }
 }
