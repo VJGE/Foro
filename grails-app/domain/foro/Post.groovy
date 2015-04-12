584297a9d4f198b14fec024cddac2382e87dceaf
Seval931 <svalencias@unal.edu.co>
2015-04-11 22:04:19 -0500

    clase post -atributos            -restricciones

--- grails-app/domain/foro/Post.groovy
+++ grails-app/domain/foro/Post.groovy
@@ -1,7 +1,21 @@
 package foro
 
 class Post {
-
+	
+	Date dateCreated
+	String topic
+	Date lastUpdate
+	boolean itsAllowed
+	Date current=new Date()
+	static transients = ['current']
+	
+	
     static constraints = {
+		dateCreated blank:false
+		topic blank:false, range:3..50
+		itsAllowed nullable:false
+		dateCreated min: current
+		lastUpdate min:current
+		
     }
 }
