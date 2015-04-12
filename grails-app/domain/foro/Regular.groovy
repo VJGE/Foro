cc7455c3dcc67c0a3e23fd905dc6c0b673970a9b
Christian <chfjimenezga@unal.edu.co>
2015-04-12 00:36:32 -0500

    Cambios iniciales clases Forum y Regular

--- grails-app/domain/foro/Regular.groovy
+++ grails-app/domain/foro/Regular.groovy
@@ -1,7 +1,16 @@
 package foro
 
-class Regular {
+class Regular extends User{
+	
+	int postViews
+	int strikesNumber
+	int starsNumbers
+	
+	static hasMany=[posts: Post]
 
     static constraints = {
+		postViews(blank:false,nullable:false,min:0)
+		strikesNumber(blank:false,nullable:false,range:0..3)
+		starsNumbers(blank:false,nullable:false,range:0..5)
     }
 }
