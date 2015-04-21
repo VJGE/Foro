fbce1765b9bb7b8dd27654392df4700224f78bab
Seval931 <svalencias@unal.edu.co>
2015-04-20 19:38:34 -0500

    update and create post and file

--- grails-app/domain/foro/Post.groovy
+++ grails-app/domain/foro/Post.groovy
@@ -8,16 +8,28 @@ class Post {
 	boolean itsAllowed
 	Date current=new Date()
 	static transients = ['current']
-	static belongsto = [regular:Regular,forum:Forum]
-	static mappedBy = [regular: "owner_id",forum:"fatherForum_id"]
+	static belongsTo = [regular:Regular,forum:Forum]
+	static mappedBy = [regular:"owner_id",forum:"fatherForum_id"]
 	static hasMany = [files:File]
 	
     static constraints = {
 		dateCreated blank:false
-		topic blank:false, range:3..50
+		topic blank:false, rangesize:3..50
 		itsAllowed nullable:false
 		dateCreated min: current
 		lastUpdate min:current
 		
     }
+	
+	def Post(String topc,boolean allowed,ownerid,forumid){
+		current=DateGroovyMethods.format(new Date(),, 'MM/dd/yyyy HH:mm:ss')
+		topic=topc
+		itsAllowed=allowed
+		regular=ownerid
+		forum=forumid
+		dateCreated=DateGroovyMethods.format(new Date(),, 'MM/dd/yyyy HH:mm:ss')
+		lastUpdate=DateGroovyMethods.format(new Date(),, 'MM/dd/yyyy HH:mm:ss')
+		
+	}
+	
 }
