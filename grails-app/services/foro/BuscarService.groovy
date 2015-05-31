bc0825e2cc1945760d3d43529da2aeff833d6ca8
Christian <chfjimenezga@unal.edu.co>
2015-05-31 11:08:36 -0500

    Modificación BuscarService

--- grails-app/services/foro/BuscarService.groovy
+++ grails-app/services/foro/BuscarService.groovy
@@ -4,8 +4,30 @@ import grails.transaction.Transactional
 
 @Transactional
 class BuscarService {
-
+    def searchableService
     def buscarForos(params){
+
+        if(!params.textoBusqueda){
+            return
+        }
+        if(params.textoBusqueda==null){
+            return
+        }else{
+            def foros=Forum.search(params.textoBusqueda,[reload: true, result: 'every'])
+            //def foros=searchableService.searchEvery(params.textoBusqueda)
+            return foros
+        }
+    }
+    def buscarUsuario(params){
+        if(!params.textoBusqueda){
+            return
+        }
+        if(params.textoBusqueda==null){
+            return
+        }else{
+            users=User.search(params.textoBusqueda,[reload: true, result:'every'])
+            return
+        }
     }
 
     def serviceMethod() {
